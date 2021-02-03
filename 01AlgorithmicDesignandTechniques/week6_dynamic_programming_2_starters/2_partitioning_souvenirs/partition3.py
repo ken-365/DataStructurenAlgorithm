30ecc02003585fed8c0dc80d7807356e2c32a161
Ken <maddid1234@gmail.com>
2021-02-03 12:35:20 -0600

    finish 01toolbox

--- 01AlgorithmicDesignandTechniques/week6_dynamic_programming_2_starters/2_partitioning_souvenirs/partition3.py
+++ 01AlgorithmicDesignandTechniques/week6_dynamic_programming_2_starters/2_partitioning_souvenirs/partition3.py
@@ -1,7 +1,10 @@
 # Uses python3
+
+#%%
+
+#%%
 import sys
 import itertools
-
 def partition3(A):
     for c in itertools.product(range(3), repeat=len(A)):
         sums = [None] * 3
@@ -13,8 +16,88 @@ def partition3(A):
 
     return 0
 
+# print(partition3([3,3,3,3]))
+# print(partition3([3,4,2]))
+# print(partition3([1, 2, 3, 4, 5, 5, 7, 7, 8, 10, 12, 19, 25]))
+#%%
+def partition(A):
+    total = sum(A)
+    if total %3 != 0 or len(A) <3 :
+        return 0
+    # each set have to equal total //3
+    answer = total //3
+    
+
+    return 'stop'
+
+# print(partition([3,3,3,3]))
+# print(partition([6,3,3,6]))
+# print(partition([30]))
+# print(partition([1, 2, 3, 4, 5, 5, 7, 7, 8, 10, 12, 19, 25]))
+
+#%%
+# for test
+import numpy
+def partitions(items):
+    """ Finds if number of partitions having capacity W is >=3
+    (int, int, list) -> (int) """
+    
+    total = sum(items)
+    if total %3 != 0 or len(items) <3 :
+        return 0
+    n = len(items)
+    W = sum(items) //  3
+    count = 0 
+    value = numpy.zeros((W+1, n+1))
+    for i in range(1, W+1):
+        for j in range(1, n+1):
+            value[i][j] = value[i][j-1]
+            # print(value)
+            if items[j-1]<=i:
+                temp = value[i-items[j-1]][j-1] + items[j-1]
+                if temp > value[i][j]:
+                    value[i][j] = temp
+            if value[i][j] == W: count += 1
+
+    if count < 3:
+        return 0
+    else:
+        return 1
+# print(partitions([3,3,3,3]))
+# print(partitions([3,4,2]))
+# print(partitions([6,3,3,6]))
+# print(partitions([30]))
+# print(partitions([1, 2, 3, 4, 5, 5, 7, 7, 8, 10, 12, 19, 25]))
+
+#%%
+def partitions_copy(W, n, items):
+    """ Finds if number of partitions having capacity W is >=3
+    (int, int, list) -> (int) """
+    count = 0 
+    value = numpy.zeros((W+1, n+1))
+    for i in range(1, W+1):
+        for j in range(1, n+1):
+            value[i][j] = value[i][j-1]
+            if items[j-1]<=i:
+                temp = value[i-items[j-1]][j-1] + items[j-1]
+                if temp > value[i][j]:
+                    value[i][j] = temp
+            if value[i][j] == W: count += 1
+
+    if count < 3: print('0')
+    else: print('1')
+
+
+#%%
+
 if __name__ == '__main__':
-    input = sys.stdin.read()
-    n, *A = list(map(int, input.split()))
-    print(partition3(A))
+    n = int(input())
+    item_weights = [int(i) for i in input().split()]
+    total_weight = sum(item_weights)
+    if n<3: 
+        print('0')
+    elif total_weight%3 != 0: 
+        print('0')
+    else:
+        partitions_copy(total_weight//3, n, item_weights)
 
--- 01AlgorithmicDesignandTechniques/week6_dynamic_programming_2_starters/3_maximum_value_of_an_arithmetic_expression/placing_parentheses.py
+++ 01AlgorithmicDesignandTechniques/week6_dynamic_programming_2_starters/3_maximum_value_of_an_arithmetic_expression/placing_parentheses.py
@@ -1,4 +1,7 @@
-# Uses python3
+
+#%%
+## Uses python3
+import numpy as np
 def evalt(a, b, op):
     if op == '+':
         return a + b
@@ -9,10 +12,50 @@ def evalt(a, b, op):
     else:
         assert False
 
+def MinAndMax(M,m,i,j,op): 
+    # where i is start index and j is end index 
+    min_value =   float('inf')
+    max_value = - float('inf')
+    for k in range(i,j):
+        a = evalt(M[i,k],M[k+1,j],op[k])
+        b = evalt(m[i,k],m[k+1,j],op[k])
+        c = evalt(M[i,k],m[k+1,j],op[k])
+        d = evalt(m[i,k],M[k+1,j],op[k])
+        min_value = min(min_value,a,b,c,d)
+        max_value = max(max_value,a,b,c,d)
+    return min_value, max_value
+
 def get_maximum_value(dataset):
-    #write your code here
-    return 0
+    number = []
+    operator =[]
+    for i in dataset:
+        if i  in ['+', '-', '*']:
+            operator.append(i)
+        else:
+            number.append(i)
+        
 
+    #init 2D numpy array
+    n = len(number)
+    M = np.zeros((n,n))
+    m = np.zeros((n,n))
+    # fill in first diaganal
+    for i in range(len(number)):
+        m[i,i] =number[i]
+        M[i,i] =number[i]
+    # start fill
+    for s in range(1,n):
+        for i in range(0,n-s):
+            # print(M)
+            # print(m)
+            j = i + s
+            m[i,j] , M[i,j] = MinAndMax(M,m,i,j,operator)
+            
+  
+    return int(M[0][n-1])
 
+# print(get_maximum_value("1+5"))
+
+# print(get_maximum_value('5-8+7*4-8+9'))
 if __name__ == "__main__":
     print(get_maximum_value(input()))
